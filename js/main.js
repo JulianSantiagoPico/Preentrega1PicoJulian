3f7c370ea231171caa018db92c38f0ef8a5274ec
JulianSantiagoPico <julpic08@gmail.com>
2022-12-16 21:51:57 -0500

    Segunda preentrega js

--- js/main.js
+++ js/main.js
@@ -1,26 +1,32 @@
+let total_areas = 4;
+let valor = 10;
+
 const suma = (a, b, c, d) => { return a + b + c + d };
 const dividir = (a,b) => {return a / b };
 const materias = { area1: "matematicas", area2: "inglés", area3: "sociales", area4: "español" };
+const areas = ["matematicas", "inglés", "sociales", "español", "recreo"];
 
-let total_areas = 4;
-let valor = 10;
-let nota_mate = parseFloat(prompt("Ingrese la nota que sacaste en " + materias.area1 + ":"));
-let nota_ing = parseFloat(prompt("Ingrese la nota que sacaste en " + materias.area2 + ":"));
-let nota_soc = parseFloat(prompt("Ingrese la nota que sacaste en " + materias.area3 + ":"));
-let nota_esp = parseFloat(prompt("Ingrese la nota que sacaste en " + materias.area4 + ":"));
+let boton = document.getElementById("btnPrincipal");
+boton.addEventListener("click", calculadora);
 
-let promedio_notaFinal = dividir(suma(nota_mate, nota_ing, nota_soc, nota_esp), total_areas);
+function calculadora () {
+    let nota_mate = parseFloat(prompt("Ingrese la nota que sacaste en " + materias.area1 + ":"));
+    let nota_ing = parseFloat(prompt("Ingrese la nota que sacaste en " + materias.area2 + ":"));
+    let nota_soc = parseFloat(prompt("Ingrese la nota que sacaste en " + materias.area3 + ":"));
+    let nota_esp = parseFloat(prompt("Ingrese la nota que sacaste en " + materias.area4 + ":"));
 
-console.log(promedio_notaFinal);
-alert( "Tu promedio de nota final es: " + promedio_notaFinal )
+    let promedio_notaFinal = dividir(suma(nota_mate, nota_ing, nota_soc, nota_esp), total_areas);
 
-if(promedio_notaFinal >= 3.5) {
+    console.log(promedio_notaFinal);
+    alert( "Tu promedio de nota final es: " + promedio_notaFinal.toFixed(2) )
+
+    if(promedio_notaFinal >= 3.5) {
     alert( "¡Felicitaciones!, lograste pasar el curso." )
-} else{
+    } else{
     alert( "Parece que vas a tener que repetir el curso." )
-}
+    }
 
-for (let i=1; i<=10; i++) {
+    for (let i=1; i<=10; i++) {
     if (valor == 1) {
         alert("¡Adios!")
         break;
@@ -28,4 +34,19 @@ for (let i=1; i<=10; i++) {
 
     valor--;
     alert("Cuenta regresiva: " + valor);
+    }
 }
+
+const eliminarArea = (area) => {
+    let indice = areas.indexOf (area);
+    console.log("Posicion: " + indice);
+
+    if (indice != -1) {
+        areas.splice(indice, 1);
+    }
+}
+
+eliminarArea("recreo");
+console.log(areas);
+
+
