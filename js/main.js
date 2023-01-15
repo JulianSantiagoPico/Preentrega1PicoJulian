d0e559516ef6df0c218e0aca7b959583cb9fdf76
JulianSantiagoPico <julpic08@gmail.com>
2023-01-14 22:09:05 -0500

    Primera version Tercera Preentrega

--- js/main.js
+++ js/main.js
@@ -3,39 +3,42 @@ let valor = 10;
 
 const suma = (a, b, c, d) => { return a + b + c + d };
 const dividir = (a,b) => {return a / b };
+
+const guardarDatos = () => {
+    let nombre = document.getElementById("nombre").value;
+    const datosUsuario = {nombreUsuario:nombre};
+    localStorage.setItem("datosUsuario", JSON.stringify(datosUsuario));
+    console.log("Datos Guardados!");
+};
+
+const recuperarDatos = () => {
+    const datosUsuario = JSON.parse(localStorage.getItem("datosUsuario"));
+    let salida = `<p>Hola de nuevo:</p>
+    <p><b>${datosUsuario.nombreUsuario}</b></p>`;
+    document.getElementById("resultado").innerHTML = salida;
+    console.log("Datos Recuperados!");
+};
+
 const materias = { area1: "matematicas", area2: "inglés", area3: "sociales", area4: "español" };
 const areas = ["matematicas", "inglés", "sociales", "español", "recreo"];
 
-let boton = document.getElementById("btnPrincipal");
-boton.addEventListener("click", calculadora);
+document.getElementById("calcular").addEventListener("click", calculadora);
+document.getElementById("guardar").addEventListener("click", guardarDatos);
+document.getElementById("cargar").addEventListener("click", recuperarDatos);
+
 
 function calculadora () {
-    let nota_mate = parseFloat(prompt("Ingrese la nota que sacaste en " + materias.area1 + ":"));
-    let nota_ing = parseFloat(prompt("Ingrese la nota que sacaste en " + materias.area2 + ":"));
-    let nota_soc = parseFloat(prompt("Ingrese la nota que sacaste en " + materias.area3 + ":"));
-    let nota_esp = parseFloat(prompt("Ingrese la nota que sacaste en " + materias.area4 + ":"));
+    let nota_mate = parseFloat(document.getElementById("mate").value);
+    let nota_ing = parseFloat(document.getElementById("inglés").value);
+    let nota_soc = parseFloat(document.getElementById("sociales").value);
+    let nota_esp = parseFloat(document.getElementById("español").value);
 
     let promedio_notaFinal = dividir(suma(nota_mate, nota_ing, nota_soc, nota_esp), total_areas);
 
     console.log(promedio_notaFinal);
-    alert( "Tu promedio de nota final es: " + promedio_notaFinal.toFixed(2) )
-
-    if(promedio_notaFinal >= 3.5) {
-    alert( "¡Felicitaciones!, lograste pasar el curso." )
-    } else{
-    alert( "Parece que vas a tener que repetir el curso." )
-    }
-
-    for (let i=1; i<=10; i++) {
-    if (valor == 1) {
-        alert("¡Adios!")
-        break;
-    }
-
-    valor--;
-    alert("Cuenta regresiva: " + valor);
-    }
-}
+    let salidaNota = `<p>Tu promedio es: <b>${promedio_notaFinal}</b></p>`;
+    document.getElementById("resultado").innerHTML = salidaNota;
+ };
 
 const eliminarArea = (area) => {
     let indice = areas.indexOf (area);
