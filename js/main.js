4c27ca5e72c1bb6253d1118dcddf150aaf6620d4
JulianSantiagoPico <julpic08@gmail.com>
2023-01-31 18:32:19 -0500

    version final

--- js/main.js
+++ js/main.js
@@ -6,28 +6,28 @@ const dividir = (a,b) => {return a / b };
 
 const guardarDatos = () => {
     let nombre = document.getElementById("nombre").value;
-    const datosUsuario = {nombreUsuario:nombre};
+    let notaGuardada = document.getElementById("notaGuardada").value;
+    const datosUsuario = {nombreUsuario:nombre, notaUsuario:notaGuardada};
     localStorage.setItem("datosUsuario", JSON.stringify(datosUsuario));
     console.log("Datos Guardados!");
 };
 
 const recuperarDatos = () => {
     const datosUsuario = JSON.parse(localStorage.getItem("datosUsuario"));
-    let salida = `<p>Hola de nuevo:</p>
-    <p><b>${datosUsuario.nombreUsuario}</b></p>`;
-    document.getElementById("resultado").innerHTML = salida;
+    let salida = `<p>Hola de nuevo: <b>${datosUsuario.nombreUsuario}</b><br> 
+    Ultima nota que guardaste: <b>${datosUsuario.notaUsuario}<b></p>`;
+    document.getElementById("resultadoCarga").innerHTML = salida;
     console.log("Datos Recuperados!");
 };
 
 const areas = ["matematicas", "inglés", "sociales", "español", "recreo"];
-const materias = [];
 
 document.getElementById("calcular").addEventListener("click", calculadora);
 document.getElementById("guardar").addEventListener("click", guardarDatos);
 document.getElementById("cargar").addEventListener("click", recuperarDatos);
 
+async function calculadora () {
 
-function calculadora () {
     let nota_mate = parseFloat(document.getElementById("mate").value);
     let nota_ing = parseFloat(document.getElementById("inglés").value);
     let nota_soc = parseFloat(document.getElementById("sociales").value);
@@ -39,6 +39,10 @@ function calculadora () {
     let salidaNota = `<p>Tu promedio es: <b>${promedio_notaFinal}</b></p>`;
     document.getElementById("resultado").innerHTML = salidaNota;
     
+    Toastify({
+        text: "Calculando...",
+        duration: 1000
+        }).showToast();
  };
 
 const eliminarArea = (area) => {
@@ -48,10 +52,10 @@ const eliminarArea = (area) => {
     if (indice != -1) {
         areas.splice(indice, 1);
     }
-}
+};
 
 eliminarArea("recreo");
-console.log(areas);
 
+console.log(areas);
 console.log(areas.length)
 
